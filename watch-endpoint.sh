7c5a0791e249c1c183ff55f33cf67d6feb036231
Siamak <ssadeghi@redhat.com>
2018-10-22 18:40:23 +0200

    print errors in the watch endpoint

--- watch-endpoint.sh
+++ watch-endpoint.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-while true; do curl http://tinyapp-demo.apps.devopsworld.openshiftworkshop.com/version; sleep 1; done
+
+URL=http://tinyapp-demo.apps.devopsworld.openshiftworkshop.com/version
+
+function call_endpoint() {
+  HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" $URL)
+  HTTP_BODY=$(echo $HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
+  HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+
+  if [ ! $HTTP_STATUS -eq 200  ]; then
+    echo "[ $(date +"%x %T") ] Error (http code: $HTTP_STATUS)"
+  else
+    echo "[ $(date +"%x %T") ] $HTTP_BODY"
+  fi
+}
+
+while true; do call_endpoint; sleep 1; done
+
